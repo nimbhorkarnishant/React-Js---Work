7925250ad3b2960d1849690e9e9da57c7c1c6428
Nishant Nimbhorkar <36923081+nimbhorkarnishant@users.noreply.github.com>
2020-07-12 16:54:08 +0530

    removing unwantated library

--- test_app/src/index.js
+++ test_app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import Login from './Authentication/login';
 import Navbar from './Authentication/navbar';
 
 import * as serviceWorker from './serviceWorker';
